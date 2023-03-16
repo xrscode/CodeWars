72a49cb52d00b4a108bfc12e9dba9220db3473e1
xrscode <120644276+xrscode@users.noreply.github.com>
2023-03-16 15:08:03 +0000

    CodeWars
    Sort Array, sum the middle numbers.  Take away the smallest and largest numbers in the array.

--- myscript.js
+++ myscript.js
@@ -1,28 +1,37 @@
 "strict";
-//Logic to test if Array and array is populated.
-let array = function (arr) {
+
+function sumArray(array) {
   let sum = 0;
-  if (Array.isArray(arr) && arr.length > 0 && !arr.some(isNaN)) {
-    for (let i = 0; i < arr.length; i++) {
-      sum = sum + arr[i];
+
+  if (Array.isArray(array) && array.length > 2 && !array.some(isNaN)) {
+    array.sort(function (a, b) {
+      return a - b;
+    });
+
+    for (let i = 1; i < array.length - 1; i++) {
+      sum = sum + array[i];
     }
   } else {
     sum = 0;
   }
-  console.log(sum);
-}; //END FUNCTION
+  console.log(`The sum of array number is ${sum}`);
+  return sum;
+}
 
 //Array Testing
 let test1 = []; //Should equal 0
 let test2 = [null]; //Should equal 0
 let test3 = [5, 3, "hat"]; //should equal 0
-let test4 = [1, 2]; // Should equal 3
-let test5 = [null, 5, 1]; //Should equal 6
-let test6 = //Should equal 0
-  //console.log(array(test2))
-  array(test1);
-array(test2);
-array(test3);
-array(test4);
-array(test5);
-array(test6);
+let test4 = [1, 2]; // Should equal 0
+let test5 = [null, 5, 1]; //Should return 1
+let test6 = [6, 2, 1, 8, 10]; //Should return 16
+let test7 = []; // should return 0
+
+//console.log(array(test2))
+sumArray(test1);
+sumArray(test2);
+sumArray(test3);
+sumArray(test4);
+sumArray(test5);
+sumArray(test6);
+sumArray(test7);
