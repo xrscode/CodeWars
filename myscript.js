360ad6035e2a8d27ce86da687e8d735bb6e08554
xrscode <120644276+xrscode@users.noreply.github.com>
2023-03-19 09:53:42 +0000

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,37 +1,16 @@
 "strict";
-
-function sumArray(array) {
-  let sum = 0;
-
-  if (Array.isArray(array) && array.length > 2 && !array.some(isNaN)) {
-    array.sort(function (a, b) {
-      return a - b;
-    });
-
-    for (let i = 1; i < array.length - 1; i++) {
-      sum = sum + array[i];
-    }
+function lovefunc(flower1, flower2) {
+  // moment of truth
+  let flower1Even = Number.isInteger(flower1 / 2);
+  let flower2Even = Number.isInteger(flower2 / 2);
+  console.log(
+    `Flower 1 is even; ${flower1Even} Flower 2 is even; ${flower2Even}`
+  );
+  if (flower1Even === flower2Even) {
+    return false;
   } else {
-    sum = 0;
+    return true;
   }
-  console.log(`The sum of array number is ${sum}`);
-  return sum;
 }
 
-//Array Testing
-let test1 = []; //Should equal 0
-let test2 = [null]; //Should equal 0
-let test3 = [5, 3, "hat"]; //should equal 0
-let test4 = [1, 2]; // Should equal 0
-let test5 = [null, 5, 1]; //Should return 1
-let test6 = [6, 2, 1, 8, 10]; //Should return 16
-let test7 = []; // should return 0
-
-//console.log(array(test2))
-sumArray(test1);
-sumArray(test2);
-sumArray(test3);
-sumArray(test4);
-sumArray(test5);
-sumArray(test6);
-sumArray(test7);
+lovefunc(1, 2);
