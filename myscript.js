0ec512cb619de4ab7a467d020126f76f1af6223e
xrscode <120644276+xrscode@users.noreply.github.com>
2023-03-19 09:59:34 +0000

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -6,11 +6,15 @@ function lovefunc(flower1, flower2) {
   console.log(
     `Flower 1 is even; ${flower1Even} Flower 2 is even; ${flower2Even}`
   );
+  console.log(`${flower1 % 2 !== flower2 % 2}`);
   if (flower1Even === flower2Even) {
     return false;
   } else {
     return true;
   }
+  console.log(`${flower1 % 2 !== flower2 % 2}`);
 }
+
+lovefunc(1, 2);
 
 lovefunc(1, 2);
