70bea3f0cc62385d3c668bb37e9cc0444382399a
xrscode <120644276+xrscode@users.noreply.github.com>
2023-03-22 10:34:26 +0000

    Triangle Theorem
    Sum of side A and B must be greater than side C.

--- myscript.js
+++ myscript.js
@@ -1,1 +1,20 @@
 "strict";
+function isTriangle(a, b, c) {
+  //(a, b, c).sort();
+  let arr = [a, b, c];
+  //console.log(a, b, c);
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+  if (arr[0] + arr[1] > arr[2] && arr[0] >= 1) {
+    console.log(`true`);
+    return true;
+  } else {
+    console.log(`false`);
+    return false;
+  }
+}
+
+//test
+isTriangle(1, 2, 3); //true
+isTriangle(7, 2, 2); //false
