eced5f5c430dfa03661e33a932e3c6c948d7e0fd
xrscode <120644276+xrscode@users.noreply.github.com>
2023-03-22 16:21:17 +0000

    Codewars Compound Interest

--- myscript.js
+++ myscript.js
@@ -1,37 +1,36 @@
 "strict";
+let P = 1000;
+let interest = 0.05;
+let tax = 0.18;
 
-function sumArray(array) {
-  let sum = 0;
+let money = function (p, I, T) {
+  return p + p * I - p * I * T;
+};
 
-  if (Array.isArray(array) && array.length > 2 && !array.some(isNaN)) {
-    array.sort(function (a, b) {
-      return a - b;
-    });
+console.log(money(1000, 0.05, 0.18));
 
-    for (let i = 1; i < array.length - 1; i++) {
-      sum = sum + array[i];
-    }
-  } else {
-    sum = 0;
-  }
-  console.log(`The sum of array number is ${sum}`);
-  return sum;
-}
+Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
 
-//Array Testing
-let test1 = []; //Should equal 0
-let test2 = [null]; //Should equal 0
-let test3 = [5, 3, "hat"]; //should equal 0
-let test4 = [1, 2]; // Should equal 0
-let test5 = [null, 5, 1]; //Should return 1
-let test6 = [6, 2, 1, 8, 10]; //Should return 16
-let test7 = []; // should return 0
+The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
 
-//console.log(array(test2))
-sumArray(test1);
-sumArray(test2);
-sumArray(test3);
-sumArray(test4);
-sumArray(test5);
-sumArray(test6);
-sumArray(test7);
+Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+Example:
+
+  Let P be the Principal = 1000.00      
+  Let I be the Interest Rate = 0.05      
+  Let T be the Tax Rate = 0.18      
+  Let D be the Desired Sum = 1100.00
+
+
+After 1st Year -->
+  P = 1041.00
+After 2nd Year -->
+  P = 1083.86
+After 3rd Year -->
+  P = 1128.30
+Thus Mr. Scrooge has to wait for 3 years for the initial principal to amount to the desired sum.
+
+Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
+
+Assumption: Assume that Desired Principal 'D' is always greater than the initial principal. However it is best to take into consideration that if Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
