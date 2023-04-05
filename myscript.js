4458aea4670056b8fdcbc4d5f843af34d18df407
xrscode <120644276+xrscode@users.noreply.github.com>
2023-04-05 09:18:00 +0100

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,19 +1,18 @@
-function copyList(l) {
-  //your code here
-  let arr = [];
-  console.log(`L is ${l}`);
-  console.log(`L is an array? ${Array.isArray(l)}`);
-  if (Array.isArray(l)) {
-    for (let i = 0; i < l.length; i++) {
-      arr.push(l[i]);
+function bouncingBall(h, bounce, window) {
+  // your code here
+  let n = 0;
+  if (bounce < 1 && h > 0) {
+    while (h > window) {
+      h = h * bounce;
+      console.log(`${n} ${h}`);
+      n++;
     }
   } else {
-    console.log("undefined");
-    return undefined;
+    return -1;
   }
-  console.log(`arr is: ${arr}`);
+
+  console.log(n);
+  return n + 1;
 }
 
-//copyList([1, 2, 3, 4]);
-copyList(1, 2, 3, 4);
-copyList([1, 29, 31, 90, 55, 88, 61, 20, 79, 30]);
+bouncingBall(30, 0.66, 1.5);
